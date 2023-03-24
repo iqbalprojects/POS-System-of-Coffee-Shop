2a85e2167f78b26755d46862c0d663e0090ca423
Iqbal <iqbaljacksonsite@gmail.com>
2023-03-24 22:39:32 +0700

    change api_url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3000/";
+export const API_URL = "https://test-json-chi.vercel.app/";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
     faHouse,
